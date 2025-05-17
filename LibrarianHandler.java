56b51c58da977c16dfb036c736c426fda41781a4
Ghada Emad <ghadaemad011@gmail.com>
2025-05-17 03:58:30 +0300

    remove handler from librarianhandler

--- LibrarianHandler.java
+++ LibrarianHandler.java
@@ -12,28 +12,4 @@ public class LibrarianHandler extends BookRequestHandler {
     }
 }
 
-class ManagerHandler extends BookRequestHandler {
-    @Override
-    protected boolean canHandle(BookRequest request) {
-        return request.getDaysRequested() > 7 && request.getDaysRequested() <= 14;
-    }
 
-    @Override
-    protected void processRequest(BookRequest request) {
-        System.out.println("Manager approves borrowing for " + request.getDaysRequested() + " days.");
-        request.getBook().borrowBook(request.getUser());
-    }
-}
-
-class DirectorHandler extends BookRequestHandler {
-    @Override
-    protected boolean canHandle(BookRequest request) {
-        return request.getDaysRequested() > 14;
-    }
-
-    @Override
-    protected void processRequest(BookRequest request) {
-        System.out.println("Director approves borrowing for " + request.getDaysRequested() + " days.");
-        request.getBook().borrowBook(request.getUser());
-    }
-}
